5bb3ba574bbb4092d51233f2597ac2d75040682f
JoelJaison394 <joeljaison394@gmail.com>
2024-01-01 09:19:00 +0530

    sol: easy/anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,17 @@
 
 function isAnagram(str1, str2) {
 
+  const cleanStr1 = str1.replace(/\s/g, '').toLowerCase();
+  const cleanStr2 = str2.replace(/\s/g, '').toLowerCase();
+
+  const srtStr1 = cleanStr1.split('').sort().join('');
+  const srtStr2 = cleanStr2.split('').sort().join('');
+
+
+  return srtStr1 === srtStr2;
 }
+
+module.exports = isAnagram;
+
 
 module.exports = isAnagram;
